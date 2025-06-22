31272105e25350e4bd02fdafe8e1543cd7260584
welington da cruz <welingtondacruz6@gmail.com>
2025-06-21 23:56:09 -0300

    .

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function playClickSound() {
       const audio = document.getElementById('clickSound');
       audio.currentTime = 0;
       audio.play();}
+
+       let pontos = 0;
+    function aumentarPontuacao() {
+      pontos++;
+      document.getElementById("points").textContent = pontos;
+    }
+
+    function toggleMute() {
+    const audio = document.getElementById('clickSound');
+    const muteBtn = document.getElementById('muteBtn');
+
+    audio.muted = !audio.muted;
+
+   if (audio.muted) {
+        muteIcon.src = "assets/gatomutado.jpg"; 
+    } else {
+        muteIcon.src = "assets/gatodesmutado.jpg"; 
+    }
+}
